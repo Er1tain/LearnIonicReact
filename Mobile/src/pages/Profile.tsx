3cda0b636e8726f584129958d826a345b1cc3c12
unknown <sanyapridava@mail.ru>
2024-07-28 04:41:06 +0300

    Add window of editing

--- Mobile/src/pages/Profile.tsx
+++ Mobile/src/pages/Profile.tsx
@@ -4,21 +4,36 @@ import '../styles/pages/Profile.css';
 import { useState } from "react";
 import { VideoCapturePlus, VideoCapturePlusOptions } from "@ionic-native/video-capture-plus";
 import styled from "styled-components";
+import Editor from "../components/Profile/Editor";
 
 const PanelButtons = styled.div`
+    margin-top: 0.5rem;
     display: flex;
     flex-direction: column;
-    
+    gap: 1rem;
+    `
 
-`
+const Button = (background: string, color: string)=>{
+   return styled.button`
+    background: ${background};
+    color: ${color};
+    font-size: 22px;
+    border-radius: 9px;
+    height: 2rem;
+   `
+}
 
 export default function Profile() {
     const nav = useIonRouter();
 
-    const Change = ()=>{
-        alert("Изменения успешно внесены!");
-    }
+    const MainButton = Button("lime", "black");
+    const ChangeButton = Button("orange", "white");
 
+    const [visibleEditor, setVisible] = useState<boolean>(false);
+    const ViewVanishEditor = ()=>{
+        if (visibleEditor) setVisible(false)
+        else setVisible(true)
+    }
     return (
         <IonPage>
             <IonHeader>
@@ -27,10 +42,10 @@ export default function Profile() {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <IonText>Настройки</IonText>
+                {visibleEditor && <Editor setVisible={setVisible}/>}
                 <PanelButtons>
-                    <IonButton color="secondary" onClick={Change}>Редактировать</IonButton>
-                    <IonButton onClick={()=>nav.goBack()}>На главную</IonButton>
+                    <MainButton onClick={()=>nav.goBack()}>На главную</MainButton>
+                    <ChangeButton onClick={()=>ViewVanishEditor()}>Редактировать</ChangeButton>
                 </PanelButtons>
             </IonContent>
         </IonPage>
